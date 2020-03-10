9abf1d66e5c1b7d45e2748abf4ffdd126a710477
Karolina Kuchta <linka_94@interia.eu>
2020-03-10 13:24:42 +0100

    Install bootstrap

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
